8fcadc7be7cfa4bd65a864e4695bde96f45a8c75
juunaska <118882741+juunaska@users.noreply.github.com>
2023-12-24 20:03:25 +0000

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,50 @@
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+ export VISUAL=nvim;
+ export EDITOR=nvim;
+
+
+
+alias porsche="sudo mount /dev/sda1 /Porsche"
+
+alias q="clear && neofetch"
+alias dd="clear"
+alias qqq="exit"
+alias sn="shutdown now"
+alias ll="ls -al"
+alias session="echo $XDG_SESSION_TYPE"
+alias vim="nvim"
+
+
+alias py="pyradio -t white_on_black"
+alias pipes="pipes.sh"
+alias hyprconf="vim ~/.config/hypr/hyprland.conf"
+alias hc="vim ~/.config/hypr/hyprland.conf"
+alias bspwmconf="vim ~/.config/bspwm/bspwmrc"
+alias bc="vim ~/.config/bspwm/bspwmrc"
+alias hyprpaperconf="vim ~/.config/hypr/hyprpaper.conf"
+alias fishconfig="vim ~/.config/fish/config.fish"
+alias fc="vim ~/.config/fish/config.fish"
+alias sweethome3d="/home/test/SweetHome3D-7.1/./SweetHome3D"
+alias session!="echo $XDG_SESSION_TYPE"
+alias neofetch="neofetch --ascii ~/.config/neofetch/ascii1.txt"
+alias update!="sudo pacman -Syu"
+alias install!="sudo pacman -S"
+alias search!="sudo pacman -Ss"
+alias walls="cd ~/Downloads/walls/ && ls"
+
+# Load Openbox stuff
+alias openbox!="nitrogen --restore & tint2"
+
+
+
+
+#wal -i ~/Downloads/walls/002.png && clear
+#echo ""
+#session!
+#neofetch
+# end
